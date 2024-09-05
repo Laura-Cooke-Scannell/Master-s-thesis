8f89d06a3b747f93c7ca7f9ae98df8d0154652a4
Laura-Cooke-Scannell <laurascannell9@gmail.com>
2024-09-05 15:18:43 -0700

    Add files via upload

---
+++ Grottaunifrac.R
@@ -0,0 +1,34 @@
+# clear all
+rm(list = ls())
+
+# set working directory to bio609 on your desktop
+setwd("/Users/Laura/Desktop/another redo of samples/nopoop")
+
+#libraries
+library(devtools)
+library(phyloseq)
+library(qiime2R)
+library(ggplot2)
+library(vegan)
+
+#create the phyloseq object
+physeq <- qza_to_phyloseq(features = "nopooptable.qza", tree = "rooted-tree.qza",
+                          taxonomy = "taxonomynopoop.qza", metadata = "metadata.csv")
+
+# PCoA plot using the unweighted UniFrac as distance
+wunifrac_dist = phyloseq::distance(physeq, method ="unifrac", weighted=F)
+ordination = ordinate(physeq, method="PCoA", distance = wunifrac_dist)
+plot_ordination(physeq, ordination, color="Location", title = "Grotta Del Cane bacterial UniFrac results P = 0.563") + theme(aspect.ratio = 1)
+
+#permanova significance test of unweighted unifrac
+adonis2(wunifrac_dist ~ sample_data(physeq)$Location)
+
+#PCoA plot using the weighted UniFrac as distance
+uunifrac_dist = phyloseq::distance(physeq, method = "unifrac", weighted=T)
+ordination2 = ordinate(physeq, method = "PCoA", distance = uunifrac_dist)
+plot_ordination(physeq, ordination2, color="Location") + theme(aspect.ratio = 1)
+
+#permanova significance test of weighted unifrac
+adonis2(uunifrac_dist ~ sample_data(physeq)$Location)
+
+
---
+++ Joostnopoopclustering.R
@@ -0,0 +1,42 @@
+# clear all
+rm(list = ls())
+
+# set working directory to nopoop on my desktop
+setwd("/Users/Laura/Desktop/Another redo of samples/Joost nopoop")
+
+#load libraries
+library(phyloseq)
+library(qiime2R)
+library(vegan)
+library(stats)
+library(dendextend)
+
+#create phyloseq object
+physeq <- qza_to_phyloseq(features = "nopoop.qza", tree = "rooted-tree.qza", 
+                          taxonomy = "taxonomy.qza", metadata = "Joostmetadata.tsv")
+
+#compute relative abundance
+ps_rel_abund = phyloseq::transform_sample_counts(physeq, function(x){x /sum(x)})
+phyloseq::otu_table(physeq)
+
+#Extract OTU table and compute bray-curtis dissimilarity 
+ps_rel_otu <- data.frame(phyloseq::otu_table(ps_rel_abund))
+ps_rel_otu <- t(ps_rel_otu)
+bc_dist <- vegan::vegdist(ps_rel_otu, method ="bray")
+as.matrix(bc_dist)
+
+#save as dendrogram
+ward <- as.dendrogram(hclust(bc_dist, method = "ward.D2"))
+
+#color codes
+meta <- data.frame(phyloseq::sample_data(ps_rel_abund))
+colorCode <- c(Interface = "hotpink3", Above.Interface= "goldenrod3", Below.Interface = "darkslategray4",
+               Negative = "red", Positive = "blue")
+labels_colors(ward) <- colorCode[meta$description][order.dendrogram(ward)]
+
+#plor
+plot(ward, main = "Sulfur Cave Bray-Curtis clustering results P = 0.07", 
+     ylab = "Bray-Curtis distance", xlab = "SampleID")
+
+#probability
+adonis2(bc_dist ~ sample_data(physeq)$description)
